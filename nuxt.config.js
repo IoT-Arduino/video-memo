dff35e08d07a5b97e80f88bde48f7fc8331c8d92
Sakayama <bunkajin@icloud.com>
2020-12-14 10:00:41 +0900

    nuxt.config spaモードに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,10 @@ const {
 } = process.env;
 
 export default {
+  mode: "spa", // デフォルトは universal
+  ssr: false, // サーバーサイドレンダリングを無効化
   // Target (https://go.nuxtjs.dev/config-target)
-  target: "static",
+  // target: "static",
 
   srcDir: "app",
 
